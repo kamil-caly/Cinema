2cb8bd33a39f08ab1a69dceded8437b321b3d028
Kamil Cały <kamil.caly@vp.pl>
2024-10-27 14:32:56 +0100

    add backend project setup

---
+++ backend/CinemaApi/CinemaApiApplication/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CinemaApiApplication.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = typeof(ServiceCollectionExtensions).Assembly;
+
+            services.AddMediatR(configuration =>
+                configuration.RegisterServicesFromAssembly(assembly));
+
+            services.AddValidatorsFromAssembly(assembly);
+
+            return services;
+        }
+    }
+}
---
+++ backend/CinemaApi/CinemaApiInfrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CinemaApiInfrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
